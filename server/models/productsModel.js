65c513e8684c72f2f00d59e9e27dd71f70bd3840
Cláudio Barroso <109551603+aubaro01@users.noreply.github.com>
2025-06-25 16:36:44 +0100

    prod
    product controller & model

--- server/models/productsModel.js
+++ server/models/productsModel.js
@@ -12,7 +12,7 @@ const productSchema = new mongoose.Schema({
     },
     peso:{
         type: String,
-        required: trusted,
+        required: true,
     },
     descricao:{
         type: String,
