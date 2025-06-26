82a159134c11edc357f30662569fca3102ee4301
Cláudio Barroso <109551603+aubaro01@users.noreply.github.com>
2025-06-26 15:27:39 +0100

    Update userRoute.js

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const {register, login} = require("../controller/authController");
 
-router.post("/user", register);
-router.post("/login", login);
+router.post("/user/register", register);
+router.post("/user/login", login);
 
 module.exports = router;
