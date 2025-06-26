7f7f653dceea2f9deb584cdc5417b0f42431d32c
Cláudio Barroso <109551603+aubaro01@users.noreply.github.com>
2025-06-26 15:27:04 +0100

    Update userRoute.js

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -2,8 +2,6 @@ const express = require("express");
 const router = express.Router();
 const {register, login} = require("../controller/authController");
 
-// Rotas para registro e auth do user
-
 router.post("/user", register);
 router.post("/login", login);
 
