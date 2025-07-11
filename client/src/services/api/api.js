63d0f85046164827a768b7c535fea5720afb3b40
Cláudio Barroso <109551603+aubaro01@users.noreply.github.com>
2025-07-11 12:53:44 +0100

    Feat add token refresh

--- client/src/services/api/api.js
+++ client/src/services/api/api.js
@@ -3,21 +3,26 @@ import axios from 'axios';
 const SERVER_URL = process.env.REACT_APP_API_BASE_URL;
 
 const api = axios.create({
-  baseURL: SERVER_URL, 
+  baseURL: SERVER_URL,
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
   },
-
-  
 });
 
 api.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem('jwt_token');
+    const refreshToken = localStorage.getItem('refreshToken');
+
     if (token) {
       config.headers['Authorization'] = `Bearer ${token}`;
     }
+
+    if (refreshToken) {
+      config.headers['Refresh-Token'] = `Bearer ${refreshToken}`; 
+    }
+
     return config;
   },
   (error) => {
