e533e4fef53596ffd477cddae22c33bdbeebe819
Cláudio Barroso <109551603+aubaro01@users.noreply.github.com>
2025-07-11 15:01:22 +0100

    Update api.js

--- client/src/services/api/api.js
+++ client/src/services/api/api.js
@@ -20,12 +20,48 @@ api.interceptors.request.use(
     }
 
     if (refreshToken) {
-      config.headers['Refresh-Token'] = `Bearer ${refreshToken}`; 
+      config.headers['Refresh-Token'] = `Bearer ${refreshToken}`;
     }
 
     return config;
   },
-  (error) => {
+  (error) => Promise.reject(error)
+);
+
+api.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const originalRequest = error.config;
+
+    if (error.response?.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+
+      try {
+        const refreshToken = localStorage.getItem('refreshToken');
+
+        const res = await axios.post(
+          `${SERVER_URL}/user/auth/refresh`,
+          {},
+          {
+            headers: {
+              'Refresh-Token': `Bearer ${refreshToken}`,
+            },
+          }
+        );
+        const newAccessToken = res.data.accessToken;
+        localStorage.setItem('jwt_token', newAccessToken);
+
+        originalRequest.headers['Authorization'] = `Bearer ${newAccessToken}`;
+
+        return api(originalRequest);
+      } catch (refreshError) {
+        localStorage.removeItem('jwt_token');
+        localStorage.removeItem('refreshToken');
+        window.location.href = '/';
+        return Promise.reject(refreshError);
+      }
+    }
+
     return Promise.reject(error);
   }
 );
